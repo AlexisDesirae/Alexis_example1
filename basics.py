83b4929f2ee4e3d1f95b1e36e6a796fc0ade70e4
AlexisDesirae <tullochad@gmail.com>
2022-04-12 21:19:33 -0400

    added functions

--- basics.py
+++ basics.py
@@ -1,9 +1,28 @@
-import random
-
-def mean (x:list):
-    x_mean = sum(x) / len(x)
+def mean (list):
+    x_mean = sum(list) / len(list)
     return x_mean
 
-def error (list,y):
-   y_errors = [i-y for i in list]
-   return y_errors
+def error(list,y):
+    y_errors = [i - y for i in list]
+    return y_errors
+
+def variance (list):
+    data_mean = mean(list)
+    sum_of_squares = sum((i-data_mean)**2 for i in list)
+    return sum_of_squares
+
+def std_dev (list, deg_of_freedom=1):
+    sum_of_sqaures = variance(list)
+    pvar = sum_of_sqaures / (len(list) - deg_of_freedom)
+    sd = pvar ** 0.5
+    return sd
+
+def covariance (list):
+    covar = 0.0
+    data_x = list;
+    data_y = list;
+    x_mean = mean(data_x)
+    y_mean = mean(data_y)
+    for i in range(len(data_x)):
+        covar += (data_x[i] - x_mean) * (data_y[i] - y_mean)
+    return covar
